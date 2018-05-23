1f4d4d5ba6c7ffdfefae19e7dfafb2eb3607549a
An1dsq <an1dsq@gmail.com>
2018-05-23 23:55:28 +0300

    This even better. As for me

--- home_work/home_work_10.py
+++ home_work/home_work_10.py
@@ -1,6 +1,6 @@
 
 def gravestone_labeler(text):
-    name = text.split('*')[0]
+    name, birth_date, death_date = text.split('*')
     birth_date = text.split('*')[1].split('-')[0]    #
     death_date = text.split('*')[2].split('-')[0]    # Не читабельно(
     lifetime = int(death_date) - int(birth_date)     # Зато работает)
