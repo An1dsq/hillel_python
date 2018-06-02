dc1d75522579bd7dd7f7b7b04b5dba914f4d5a46
An1dsq <an1dsq@gmail.com>
2018-06-02 10:34:41 +0300

    Attentively read the task & corrected

--- home_work/home_work_25.py
+++ home_work/home_work_25.py
@@ -1,15 +1,15 @@
 import random
 
 
-def shuffle_list(lst): # no return (shuffles list in place)
+def shuffle_list(lst):  # no return (shuffles list in place)
     for i in range(len(lst) - 1):
         j = random.randint(0, len(lst) - 1)
         lst[i], lst[j] = lst[j], lst[i]
 
 
-# list1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-# print(id(list1))
-# print(list1)
-# shuffle_list(list1)
-# print(list1)
-# print(id(list1))
+list1 = list(range(1, 101, 2))
+print(id(list1))
+print(list1)
+shuffle_list(list1)
+print(list1)
+print(id(list1))
