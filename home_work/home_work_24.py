defdde3c36702b00a62fd15273e0763e47fa95e4
An1dsq <an1dsq@gmail.com>
2018-06-06 10:18:52 +0300

    And even more

--- home_work/home_work_24.py
+++ home_work/home_work_24.py
@@ -3,12 +3,12 @@ def group_by_surname(list_of_enrollees):  # returns 4 ints
     a_i_group = j_p_group = q_t_group = u_z_group = 0
     for f_name in list_of_enrollees:
         name_char = (f_name.split()[1])[0]
-        if 'A' <= name_char <='I':
+        if 'A' <= name_char <= 'I':
             a_i_group += 1
-        elif 'J' <= name_char <='P':
+        elif 'J' <= name_char <= 'P':
             j_p_group += 1
-        elif 'Q' <= name_char <='T':
+        elif 'Q' <= name_char <= 'T':
             q_t_group += 1
-        elif 'Q' <= name_char <='Z':
+        elif 'Q' <= name_char <= 'Z':
             u_z_group += 1
     return a_i_group, j_p_group, q_t_group, u_z_group
