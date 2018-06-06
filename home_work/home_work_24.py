4c44edfd7d751d1b6b59884a06ab72d271fbb876
An1dsq <an1dsq@gmail.com>
2018-06-06 10:18:11 +0300

    Just prettify

--- home_work/home_work_24.py
+++ home_work/home_work_24.py
@@ -3,12 +3,12 @@ def group_by_surname(list_of_enrollees):  # returns 4 ints
     a_i_group = j_p_group = q_t_group = u_z_group = 0
     for f_name in list_of_enrollees:
         name_char = (f_name.split()[1])[0]
-        if ord(name_char) in range(ord('A'), ord('I') + 1):
+        if 'A' <= name_char <='I':
             a_i_group += 1
-        elif ord(name_char) in range(ord('J'), ord('P') + 1):
+        elif 'J' <= name_char <='P':
             j_p_group += 1
-        elif ord(name_char) in range(ord('Q'), ord('T') + 1):
+        elif 'Q' <= name_char <='T':
             q_t_group += 1
-        elif ord(name_char) in range(ord('U'), ord('Z') + 1):
+        elif 'Q' <= name_char <='Z':
             u_z_group += 1
     return a_i_group, j_p_group, q_t_group, u_z_group
