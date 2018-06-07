74da061fda68be24154c952bec2b5cfbe6d64534
An1dsq <an1dsq@gmail.com>
2018-06-07 12:03:55 +0300

    I am fully satisfied with my "handmade" version of handling with punctuation. I consider it as addition facultative task (just for fun). So I'll keep it as is.

--- home_work/home_work_27.py
+++ home_work/home_work_27.py
@@ -5,11 +5,12 @@ import string
 def pemrtuate(text):  # returns permuted text
     words = [word for word in text.split()]
     result = []
+    skipped_word_len = 3
     for word in words:
-        if len(word) > 3:
-            if word[len(word) - 1] in string.punctuation:
-                after_word = word[len(word) - 1]
-                new_word = str(letters_mixer(word[:len(word) - 1])) + after_word
+        if len(word) > skipped_word_len:
+            if word[-1] in string.punctuation:
+                after_word = word[-1]
+                new_word = str(letters_mixer(word[:-1])) + after_word
                 result.append(new_word)
             else:
                 result.append(letters_mixer(word))
@@ -19,16 +20,18 @@ def pemrtuate(text):  # returns permuted text
 
 
 def letters_mixer(word):
-    first_char, part_to_mix, last_char = word[0], list(word[1:len(word) - 1]), word[len(word) - 1]
+    first_char, part_to_mix, last_char = word[0], list(word[1:-1]), word[-1]
     k = 0
+    min_allowed_len = 2
+    step = 2
     while 1 + k < len(part_to_mix):
-        if len(part_to_mix) > 2:
-            x = part_to_mix[k:k + 2]
+        if len(part_to_mix) > min_allowed_len:
+            x = part_to_mix[k:k+step]
             random.shuffle(x)
-            part_to_mix[k:k+2] = x
+            part_to_mix[k:k+step] = x
         else:
             random.shuffle(part_to_mix)
-        k += 2
+        k += step
     return first_char + ''.join(part_to_mix) + last_char
 
 
