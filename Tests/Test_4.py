cca071b5a33f4643884b554137c11828c7d05d7f
An1dsq <an1dsq@gmail.com>
2018-06-08 16:58:51 +0300

    Done

--- Tests/Test_4.py
+++ Tests/Test_4.py
@@ -1,9 +1,12 @@
-approved_chars = [str(i) for i in range(10)]
 number = input('Введи пятизначное число: ')
 result = 1
+odd_count = 0
 for letter in number:
-    if letter in approved_chars:
         digit = int(letter)
         if digit % 2 != 0:
             result *= digit
-print('Произведение нечётных цифр:', result)
+            odd_count += 1
+if odd_count == 0:
+    print('Барабанил барабанил, а по нечётным так и не попал')
+else:
+    print('Произведение нечётных цифр:', result)
