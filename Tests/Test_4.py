f6393b0bc9c0f8c75e877fd4e7d0b7718a229a8e
An1dsq <an1dsq@gmail.com>
2018-06-08 17:00:41 +0300

    Done

--- Tests/Test_4.py
+++ Tests/Test_4.py
@@ -2,6 +2,7 @@ number = input('Введи пятизначное число: ')
 result = 1
 odd_count = 0
 for letter in number:
+    if letter.isnumeric():
         digit = int(letter)
         if digit % 2 != 0:
             result *= digit
