17aed66f29153970e89d35c9cf4bc7a30d4b9226
An1dsq <an1dsq@gmail.com>
2018-06-09 09:31:53 +0300

    I don't know how to this. That's why for now I'll keep it as it suppose to be written. But it bothers me, so I'll keep trying to rind the solution. If I fail, I'ii ask you a hint.

--- Tests/Test_11.py
+++ Tests/Test_11.py
@@ -2,26 +2,26 @@ def exotic_sorter(lst):
     for i in range(len(lst)):
         for j in range(len(lst[i]) - 2):
             if i % 2 == 0:
-                sort(lst[i], 0)
+                lst[i].sort(reverse=True)
             else:
-                sort(lst[i], 1)
+                lst[i].sort()
     return lst
 
 
-def sort(lst, reverse=0):
-    for i in range(len(lst) - 1):
-        for _ in lst:
-            if reverse:
-                if lst[i] > lst[i+1]:
-                    lst[i],  lst[i+1] = lst[i+1], lst[i]
-                    if i + 1 != len(lst) - 1:
-                        i += 1
-            else:
-                if lst[i] < lst[i+1]:
-                    lst[i],  lst[i+1] = lst[i+1], lst[i]
-                    if i + 1 != len(lst) - 1:
-                        i += 1
-    return lst
+# def sort(lst, reverse=0):
+#     for i in range(len(lst) - 1):
+#         for _ in lst:
+#             if reverse:
+#                 if lst[i] > lst[i+1]:
+#                     lst[i],  lst[i+1] = lst[i+1], lst[i]
+#                     if i + 1 != len(lst) - 1:
+#                         i += 1
+#             else:
+#                 if lst[i] < lst[i+1]:
+#                     lst[i],  lst[i+1] = lst[i+1], lst[i]
+#                     if i + 1 != len(lst) - 1:
+#                         i += 1
+#     return lst
 
 
 unsorted_list = [[1, 3, 2], [4, 6, 5], [7, 9, 8], [10, 13, 12], [14, 16, 15], [17, 19, 18]]
