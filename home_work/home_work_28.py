b4acce385a9a2db2f33526a4d563772d3f981f2a
An1dsq <an1dsq@gmail.com>
2018-06-10 11:50:26 +0300

    Please check

---
+++ home_work/home_work_28.py
@@ -0,0 +1,22 @@
+import string
+
+
+def encode(str_to_encode):  # returns enсoded string
+    table = string.ascii_lowercase + string.digits
+    encoded_str = ''
+    step = 5
+    for i in range(len(str_to_encode)):
+        if str_to_encode[i].lower() in table:
+            index = table.find(str_to_encode[i].lower())
+            if index >= len(table) - step:
+                index = abs(step - (len(table) - index))
+                encoded_str += table[index]
+            else:
+                encoded_str += table[index + step]
+        else:
+            encoded_str += str_to_encode[i]
+    return encoded_str
+
+
+print(encode('secret'))
+print(encode('Office 365'))
