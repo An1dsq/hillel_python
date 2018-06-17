f3905d2261d24b8d29f1b6200bc2598e6bfb89dc
An1dsq <an1dsq@gmail.com>
2018-06-17 16:09:00 +0300

    I've done it your way

--- Tests/Test_9.py
+++ Tests/Test_9.py
@@ -1,8 +1,12 @@
 
 def list_archiver(lst, lower_bound, upper_bound):
+
+    old_len = abs(lower_bound) + abs(upper_bound)
+    new_len = abs(min(lst)) + abs(max(lst))
+    coef = round(old_len / new_len, 1)
     result = []
     for elem in lst:
-        new_value = ((elem - min(lst)) / (max(lst) - min(lst))) * (upper_bound - lower_bound) + lower_bound
+        new_value = elem * coef
         result.append(round(new_value, 2))
     return result
 
